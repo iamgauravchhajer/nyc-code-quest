a757a929e10ef57f75fa04aaf86e17e763cf309f
Bhavya-Dhanawani <bhavyadhanwani1234@gmail.com>
2026-07-20 14:33:01 +0530

    Added the auth middleware

---
+++ server/src/shared/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+// importing modules
+import jwt from "jsonwebtoken";
+import ApiError from "../utils/apiError.util.js";
+import env from "../config/env.config.js";
+
+function authMiddleware(req, res, next) {
+
+    const token = req.cookies.token;
+
+    if (!token) {
+        throw new ApiError(401, "Unauthorized");
+    }
+
+    try {
+        const decoded = jwt.verify(token, env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }
+    catch (err) {
+        throw new ApiError(401, "Unauthorized");
+    }
+
+    next();
+
+}
