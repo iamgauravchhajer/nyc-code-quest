7a74cf6d54e1d0049bd491bc66be72243f0cf0c0
Bhavya-Dhanawani <bhavyadhanwani1234@gmail.com>
2026-07-20 17:23:33 +0530

    sent port to 3000

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://server:5000",
+        target: "http://server:3000",
         changeOrigin: true,
       },
     },
