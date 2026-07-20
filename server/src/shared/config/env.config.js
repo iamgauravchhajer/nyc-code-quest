5710b8ce0ed9c297514fcdd9793a89d84158a493
Bhavya-Dhanawani <bhavyadhanwani1234@gmail.com>
2026-07-20 13:09:29 +0530

    ENV configured

---
+++ server/src/shared/config/env.config.js
@@ -0,0 +1,32 @@
+// Imoprting modules 
+import { config } from "dotenv";
+import z from "zod";
+import envConstants from "../constants/env.constant.js";
+import e from "express";
+
+// Load environment variables from .env file
+config();
+
+// Define a schema for environment variables using zod
+const envSchema = z.object({
+    NODE_ENV: z.enum(["development", "production", "test"]).default(envConstants.NODE_ENV),
+    PORT: z.coerce.number().default(envConstants.PORT),
+    MONGO_URI: z.string().default(envConstants.MONGO_URI),
+    SMTP_MAIL: z.coerce.boolean().default(envConstants.SMTP_MAIL),
+    LOGGER_LEVEL: z.string().default(envConstants.LOGGER_LEVEL),
+});
+
+// Parse and validate environment variables
+const parsedEnv = envSchema.safeParse(process.env);
+
+// If validation fails, throw an error with details
+if (!parsedEnv.success) {
+    console.error("Invalid environment variables:", parsedEnv.error.format());
+    process.exit(1); // Exit the process with an error code
+}
+
+// making the enc 
+const env = parsedEnv.data;
+
+// Export the validated environment variables
+export default env;
---
+++ server/src/shared/constants/env.constant.js
@@ -0,0 +1,9 @@
+const envConstants = {
+    NODE_ENV: "development",
+    PORT: 5000,
+    MONGO_URI: "mongodb+srv://localhost:27017/nyc",
+    SMTP_MAIL: false,
+    LOGGER_LEVEL: "info",
+}
+
+export default envConstants;
